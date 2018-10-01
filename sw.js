d6f89442b4d199d10237b48dba0c0c587a82c3af
Autumn <autumn.m.peters@gmail.com>
2018-10-01 10:55:28 -0700

    FixSW

--- sw.js
+++ sw.js
@@ -1,6 +1,34 @@
 /*Resubmission*/
 
 self.addEventListener('fetch', function (event) {
+  event.waitUntil(
+    caches.open('rest-static').then(function(cache){
+      return cache.addAll([
+        '/',
+        'index.html',
+        'restaurant.html',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'js/main.js',
+        'data/restaurants.json'
+      ]);
+    })
+  );
+});
+
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        if (response) return response;
+        return fetch(event.request);
+      })
+    );
+  });
+
+
+/*self.addEventListener('fetch', function (event) {
     event.respondWith(caches.match(event.request, {ignoreSearch: true}).then(function (db_response) {
         //Returns repsonse if cache is found
         if (db_response && db_response.type !== "undefined") {
@@ -20,5 +48,4 @@ self.addEventListener('fetch', function (event) {
             return response;
         })
     }));
-});
-
+}); */
