3c97ea193cb9ee65708fde418388ef4e5418496a
Autumn <autumn.m.peters@gmail.com>
2018-10-06 10:07:41 -0700

    deleteRestaurantsjson

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
 /*Resubmission*/
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.match(event.request, {ignoreSearch: true}).then(function (db_response) {
-        //Returns repsonse if cache is found
-        if (db_response && db_response.type !== "undefined") {
-            console.log("found in db: ", event.request);
-            return db_response;
-        }
-
+/*
         return fetch(event.request).then(function (response) {
             console.log("made a fetch");
             let responseToCache = response.clone();
@@ -21,4 +14,4 @@ self.addEventListener('fetch', function (event) {
         })
     }));
 });
-
+*/
